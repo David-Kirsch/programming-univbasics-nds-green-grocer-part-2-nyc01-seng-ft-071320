d4ccfedb6dbc86bd1c35d7a0b4e388320300d66a
David Kirsch <davidjkirsch@gmail.com>
2020-06-29 18:49:48 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -13,11 +13,14 @@ def apply_coupons(cart, coupons)
          coupon[:price] = discount_item[:cost] / discount_item[:num]
          coupon[:clearance] = cart_item[:clearance]
          coupon[:count] = discount_item[:num]
-         binding.pry
        end
        cart.push coupon
      end
+     binding.pry
    end
+    binding.pry
+   cart
+  
 end
 
 def apply_clearance(cart)
