04024cebf560e9c7af764a34ec34b9e61552d513
David Kirsch <davidjkirsch@gmail.com>
2020-06-29 18:28:52 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,7 +4,7 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  
+  binding.pry
 end
 
 def apply_clearance(cart)
